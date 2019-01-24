af8e08dbd1b27cdc902949171c00b44427d650cb
= <=>
2019-01-24 16:31:05 +0500

    functions completed

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,8 @@
 const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 const createUser = require('./create_user');
-const requestOneTimePassword = require('./request_one_time_password')
+const requestOneTimePassword = require('./request_one_time_password');
+const verifyOneTimePassword = require('./verify_one_time_password');
 const serviceAccount = require('./service_account.json');
 
 admin.initializeApp({
@@ -11,3 +12,4 @@ admin.initializeApp({
 
 exports.createUser = functions.https.onRequest(createUser);
 exports.requestOneTimePassword = functions.https.onRequest(requestOneTimePassword);
+exports.verifyOneTimePassword = functions.https.onRequest(verifyOneTimePassword);
